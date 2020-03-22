833546ad80df07ffdbdc5b73b99835aedd236d07
Asim <ask2asim@gmail.com>
2020-03-22 13:08:37 +0600

    updated

--- KeyboardTest1/ContentView.swift
+++ KeyboardTest1/ContentView.swift
@@ -1,39 +1,30 @@
-//
-//  ContentView.swift
-//  KeyboardTest1
-//
-//  Created by Asim Das on 21/3/20.
-//  Copyright © 2020 Asim Das. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
             
+            Text("General > Keyboard > Keyboards > Add Keyboard")
+                .foregroundColor(Color.gray)
+                .font(.system(size: 12))
+                .padding()
+            
+            
             Button(action: {
                 
                 UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
                 
-            }) {
-                Text("Set Keyboard First")
+            }) { Text("Setup Keyboard")
                     .font(.title)
                     .foregroundColor(.white)
                     .padding()
                     .background(Color.purple)
             }
-            
-            
-            Text("General > Keyboard > Keyboards > Add Keyboard")
-                .foregroundColor(Color.purple)
-                .padding()
-            
-            
-            
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- MyKeyboard/KeyboardViewController.swift
+++ MyKeyboard/KeyboardViewController.swift
@@ -1,69 +1,43 @@
-//import UIKit
 import SwiftUI
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
-    
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
-        
-        // Add custom view sizing constraints here
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let child = UIHostingController(rootView: MyKeyButtons())
-         //that's wrong, it must be true to make flexible constraints work
-        // child.translatesAutoresizingMaskIntoConstraints = false
-         child.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-         view.addSubview(child.view)
-         addChild(child)//not sure what is this for, it works without it.
-        
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
-        
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-    }
-    
-    override func viewWillLayoutSubviews() {
-        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
-        super.viewWillLayoutSubviews()
-    }
-    
-    override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
-    }
-    
-    override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+        let vc = UIHostingController(rootView: MyKeyButtons())
+        vc.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(vc.view)
     }
     
     
     //==================================
     func keyPressed() {
-          print("test--- clicked! ")
-          //textDocumentProxy.insertText("a")
-          (textDocumentProxy as UIKeyInput).insertText("a")
-      }
+        print("test--- clicked! ")
+        //textDocumentProxy.insertText("a")
+        (textDocumentProxy as UIKeyInput).insertText("a")
+    }
+    
+}
 
+
+struct MyKeyButtons: View {
+    
+    let data: [String] = ["A", "B", "C"]
+    var body: some View {
+        HStack {
+            ForEach(data, id: \.self) { aData in
+                
+                Button(action: {
+                    print("button pressed!")
+                    //keyPressed()
+                    
+                }) {
+                    Text(aData).fontWeight(.bold).font(.title)
+                        .foregroundColor(.white).padding()
+                        .background(Color.purple)
+                }
+            }
+        }
+    }
 }
